03a7689449046787bf8e0f14e43325fb78d11c50
Prasenjit Singha Roy <prasenjitsingharoy17@gmail.com>
2022-10-25 19:14:08 +0530

    Deleted custom_input.txt and .DS_Store files

--- geektrust.js
+++ geektrust.js
@@ -4,7 +4,7 @@ const Train = require('./train');
 
 // extracting the filename and reading it
 const filename = process.argv[2];
-data = fs.readFileSync(filename).toString();
+const data = fs.readFileSync(filename).toString();
 
 // creating instance of vehicle class (named Train while importing) and calling the member function 'main'
 let train = new Train();
